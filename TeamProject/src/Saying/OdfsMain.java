c52e7cae51de51e74aa079b176173b293ffb2cdb
admin <admin@YD03-11>
2020-06-05 17:44:56 +0900

    1

---
+++ TeamProject/src/Saying/OdfsMain.java
@@ -0,0 +1,27 @@
+package Saying;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class OdfsMain extends Application
+{
+
+	@Override
+	public void start(Stage arg0) throws Exception
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+		Scene scene = new Scene(root);
+
+		arg0.show();
+		arg0.setScene(scene);
+	}
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+
+}
