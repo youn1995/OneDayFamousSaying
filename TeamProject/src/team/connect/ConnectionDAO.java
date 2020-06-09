c656d56a52cfb4bbbfd37ebc7f1a4f6227a30de2
SungyounKim <sg17990@gmail.com>
2020-06-09 12:20:33 +0900

    add

---
+++ TeamProject/src/team/connect/ConnectionDAO.java
@@ -0,0 +1,31 @@
+package team.connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ConnectionDAO {
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+
+	public Connection getConnect() { // 세션접속
+
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url, "hr", "hr");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	
+	
+	
+	
+	
+}
--- TeamProject/src/team/control/MainController.java
+++ TeamProject/src/team/control/MainController.java
@@ -1,6 +1,22 @@
 package team.control;
 
-public class MainController
-{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class MainController implements Initializable {
+	int UserId;
+	
+	@FXML
+	Button btnMyList;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
 
 }
