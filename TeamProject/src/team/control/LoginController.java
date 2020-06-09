e7efee5eb75e7840df6fa6637701271d77ae9e7b
admin <admin@YD03-11>
2020-06-09 12:47:45 +0900

    1

--- TeamProject/src/team/control/LoginController.java
+++ TeamProject/src/team/control/LoginController.java
@@ -1,6 +1,24 @@
 package team.control;
 
-public class LoginController
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController implements Initializable
 {
+	@FXML Button buttonSignin, buttonSignup, buttonCancel, buttonSignupComp;
+	@FXML TextField textEmail, textName, textSignId, textid;
+	@FXML PasswordField textSignPassword, textPassword;
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1)
+	{
+		
+		
+	}
 
 }
